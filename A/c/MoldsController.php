194d14a64e65ed02b7261c2fc0511f3dee0df475
Cherry-toto <2581047041@qq.com>
2019-11-23 10:38:18 +0800

    增加模块列表以ID排序

--- A/c/MoldsController.php
+++ A/c/MoldsController.php
@@ -23,7 +23,7 @@ class MoldsController extends CommonController
 		$page = new Page('Molds');
 		$sql = ' 1=1 ';
 
-		$data = $page->where($sql)->page($this->frparam('page',0,1))->go();
+		$data = $page->where($sql)->orderby('id asc')->page($this->frparam('page',0,1))->go();
 		$pages = $page->pageList();
 		$this->pages = $pages;
 		$this->lists = $data;
